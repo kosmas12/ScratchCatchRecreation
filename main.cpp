8875138ed89cdeeafe231b9580c2193fef201477
Kosmas Raptis <keeperkosmas6@gmail.com>
2021-01-10 16:34:38 +0200

    Implement scoring and fix CMake

--- main.cpp
+++ main.cpp
@@ -17,9 +17,21 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
 
 #include <iostream>
+#if defined(NXDK)
+#include <hal/video.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#define HOME "D:\\"
+#else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#define HOME "./"
+#endif
 #include <chrono>
+#include <random>
+#include <sstream>
 
 #define WIDTH 720
 #define HEIGHT 480
@@ -33,9 +45,16 @@ static SDL_Rect circlePosition;
 static SDL_Color green;
 static SDL_Color red;
 static SDL_Color blue;
-static int steps = 10;
+static int ballSteps = 25;
+static int paddleSteps = 15;
 static SDL_Surface *paddle;
 static int radius = 20;
+static SDL_Color colors[3];
+static int currentColorNum = 0;
+static SDL_Color currentColor;
+static int score = 0;
+static TTF_Font *font;
+
 
 SDL_Surface *LoadBackground(const char *path) {
   SDL_Surface *background;
@@ -50,28 +69,28 @@ void drawPaddle(SDL_Rect position, SDL_Color color, int direction) {
   SDL_Rect prevPosition;
   //direction 0 = left, direction 1 = right
   if (direction == 0) {
-    prevPosition.x = position.x + steps;
-    prevPosition.w = position.w + steps;
+    prevPosition.x = position.x + paddleSteps;
+    prevPosition.w = position.w + paddleSteps;
   }
   else {
-    prevPosition.x = position.x - steps;
-    prevPosition.w = position.w - steps;
+    prevPosition.x = position.x - paddleSteps;
+    prevPosition.w = position.w - paddleSteps;
   }
   prevPosition.y = position.y;
   prevPosition.h = position.h;
   SDL_BlitSurface(backgroundImage, &prevPosition, paddle, NULL);
   SDL_BlitSurface(paddle, NULL, windowSurface, &prevPosition);
-  SDL_FillRect(paddle, NULL, SDL_MapRGBA(paddle->format, color.r, color.g, color.b, 255));
+  SDL_FillRect(paddle, NULL, SDL_MapRGB(paddle->format, color.r, color.g, color.b));
   SDL_BlitSurface(paddle, NULL, windowSurface, &position);
   SDL_UpdateWindowSurface(window);
 }
 
 void eraseOldCircle() {
-  SDL_Rect oldPos = {circlePosition.x-(radius+steps), circlePosition.y-(radius+steps), radius*radius, radius*2+steps};
+  SDL_Rect oldPos = {circlePosition.x-(radius+paddleSteps), circlePosition.y-(radius+paddleSteps), radius*radius, radius*2+paddleSteps};
   SDL_BlitSurface(backgroundImage, &oldPos, windowSurface, &oldPos);
 }
 
-void Circle(int center_x, int center_y, int radius, SDL_Color color){
+void Circle(int center_x, int center_y, int radius, SDL_Color color) {
   if (center_y == paddlePosition.y) {
     center_y = 30;
   }
@@ -98,23 +117,66 @@ void Circle(int center_x, int center_y, int radius, SDL_Color color){
 void PutResourcesToScreen() {
   SDL_BlitSurface(backgroundImage, 0, windowSurface, 0);
   drawPaddle(paddlePosition, green, 0);
-  Circle(circlePosition.x, circlePosition.y, 20, green);
   SDL_UpdateWindowSurface(window);
 }
 
-void Init() {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  IMG_Init(IMG_INIT_PNG);
+int Init() {
+
+#if defined(NXDK)
+  XVideoSetMode(WIDTH, HEIGHT, 32, REFRESH_DEFAULT);
+#endif
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS|SDL_INIT_TIMER) != 0) {
+    std::cout << "Couldn't initialize SDL! Reason: " << SDL_GetError() << std::endl;
+    exit(1);
+  }
+  if (IMG_Init(IMG_INIT_PNG) < 0) {
+    std::cout << "Couldn't initialize SDL_image! Reason: " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    exit(2);
+  }
+  if (TTF_Init() != 0) {
+    std::cout << "Couldn't initialize SDL_ttf! Reason: " << SDL_GetError() << std::endl;
+    IMG_Quit();
+    SDL_Quit();
+    exit(3);
+  }
+
+  font = TTF_OpenFont(HOME"Roboto-Regular.ttf", 20);
+
+  if (!font) {
+    std::cout << "Couldn't initialize font! Reason: " << SDL_GetError() << std::endl;
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+    exit(4);
+  }
+
   window = SDL_CreateWindow("ScratchCatchRecreation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
   windowSurface = SDL_GetWindowSurface(window);
-  backgroundImage = LoadBackground("background.png");
+  backgroundImage = LoadBackground(HOME"background.png");
   paddlePosition = {250, 390, 180, 50};
-  circlePosition = {345, 30, 0, 0}; // Width and height don't matter here
-  paddle = SDL_CreateRGBSurface(0, 180, 50, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+  circlePosition = {345, 70, 0, 0}; // Width and height don't matter here
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+  Uint32 Rmask = 0x000000FF;
+  Uint32 Gmask = 0x0000FF00;
+  Uint32 Bmask = 0x00FF0000;
+  Uint32 Amask = 0xFF000000;
+#else
+  Uint32 Amask = 0x000000FF;
+  Uint32 Rmask = 0x0000FF00;
+  Uint32 Gmask = 0x00FF0000;
+  Uint32 Bmask = 0xFF000000;
+#endif
+  paddle = SDL_CreateRGBSurface(0, 180, 50, 32, Rmask, Gmask, Bmask, Amask);
   green = {0, 92, 3};
   red = {209, 15, 15};
   blue = {0, 50, 130};
+  colors[0] = green;
+  colors[1] = red;
+  colors[2] = blue;
+  currentColor = colors[currentColorNum];
   PutResourcesToScreen();
+  return 0;
 }
 
 void Quit(int status) {
@@ -137,12 +199,21 @@ void ProcessInput() {
       case SDL_KEYDOWN:
         switch (event.key.keysym.sym) {
           case SDLK_LEFT:
-            paddlePosition.x-=steps;
+            paddlePosition.x-=10;
             drawPaddle(paddlePosition, green, 0);
             break;
           case SDLK_RIGHT:
-            paddlePosition.x+=steps;
+            paddlePosition.x+=10;
             drawPaddle(paddlePosition, green, 1);
+            break;
+          case SDLK_SPACE:
+            if (currentColorNum < 2) {
+              currentColorNum++;
+            }
+            else {
+              currentColorNum = 0;
+            }
+            currentColor = colors[currentColorNum];
             break;
         }
         break;
@@ -150,18 +221,73 @@ void ProcessInput() {
   }
 }
 
+Uint32 get_pixel32(SDL_Surface *surface, int x, int y ) {
+  SDL_LockSurface(surface);
+  //Convert the pixels to 32 bit
+  Uint32 *pixels = (Uint32 *)surface->pixels;
+
+  //Get the requested pixel
+  return pixels[ ( y * surface->w ) + x ];
+}
+
+int compare(SDL_Surface *paddle, SDL_Surface *circle) {
+  SDL_Color circleRGB;
+  Uint32 circlePixel = get_pixel32(circle, circlePosition.x, circlePosition.y);
+  SDL_UnlockSurface(circle);
+  SDL_GetRGB(circlePixel, windowSurface->format, &circleRGB.r, &circleRGB.g, &circleRGB.b);
+  SDL_Color paddleRGB;
+  Uint32 paddlePixel = get_pixel32(paddle, 10, 10);
+  SDL_UnlockSurface(paddle);
+  SDL_GetRGB(paddlePixel, paddle->format, &paddleRGB.r, &paddleRGB.g, &paddleRGB.b);
+  if (paddleRGB.r == circleRGB.r && paddleRGB.g == circleRGB.g && paddleRGB.b == circleRGB.b) {
+    return 1;
+  }
+  return 0;
+}
+
+void startNewCircle() {
+  eraseOldCircle();
+  circlePosition.y = 60;
+  srand(time(NULL));
+  circlePosition.x = (rand() + radius*2) % 620;
+}
+
+void PrintScore() {
+  SDL_Rect scorePos = {0, 0, 140, 20};
+  SDL_BlitSurface(backgroundImage, &scorePos, windowSurface, &scorePos);
+  std::stringstream scoreStream;
+  scoreStream << "Score: " << score;
+  std::string scoreString = scoreStream.str();
+  SDL_Surface *score = TTF_RenderText_Blended(font, scoreString.c_str(), red);
+  scoreStream.str("");
+  SDL_BlitSurface(score, NULL, windowSurface, &scorePos);
+}
+
+void ChangeScore() {
+  if (compare(paddle, windowSurface)) {
+    score++;
+  }
+  else {
+    if (score > 0) {
+      score--;
+    }
+  }
+}
+
 int main() {
   Init();
   while(!quitted) {
+    PrintScore();
+    startNewCircle();
+    SDL_Color circleColor = colors[rand() % 3];
     while (circlePosition.y + radius < paddlePosition.y) {
-      drawPaddle(paddlePosition, green, 0);
-      SDL_Delay(50/steps); //
-      circlePosition.y++;
-      Circle(circlePosition.x, circlePosition.y, radius, red);
+      drawPaddle(paddlePosition, currentColor, 0);
+      SDL_Delay(50/ballSteps);
+      Circle(circlePosition.x, circlePosition.y, radius, circleColor);
       ProcessInput();
+      circlePosition.y++;
     }
-    eraseOldCircle();
-    circlePosition.y = 30;
+    ChangeScore();
   }
   Quit(0);
   return 0;
