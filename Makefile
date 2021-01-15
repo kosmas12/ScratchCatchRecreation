8552bf33b99207f533610c5af8fc1a655364036f
Kosmas Raptis <keeperkosmas6@gmail.com>
2021-01-15 09:53:25 +0200

    Greatly improve performance and change background image resolution to 640x480

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++-8 main.cpp -o ScratchCatchRecreation -lSDL2 -I/usr/include/SDL2 -lSDL2_image -lSDL2_ttf -I/usr/include/SDL2 -std=c++17 -Wall -pedantic -g -Og
+	g++-8 main.cpp -o ScratchCatchRecreation -lSDL2 -I/usr/include/SDL2 -lSDL2_image -lSDL2_ttf -I/usr/include/SDL2 -std=c++17 -Wall -Wextra -pedantic -g -Og
--- main.cpp
+++ main.cpp
@@ -48,7 +48,6 @@ static SDL_Color red;
 static SDL_Color blue;
 static int ballSteps = 25;
 static int paddleSteps = 15;
-static SDL_Surface *paddle;
 static int radius = 20;
 static SDL_Color colors[3];
 static int currentColorNum = 0;
@@ -80,56 +79,110 @@ SDL_Surface *LoadBackground(const char *path) {
   return optimizedBackground;
 }
 
+void GetBGToFG(SDL_Rect pos) {
+
+  uint32_t *windowPixels = (uint32_t *) windowSurface->pixels;
+  SDL_LockSurface(windowSurface); // Lock surface for direct pixel access capability
+  uint32_t *bgPixels = (uint32_t *) backgroundImage->pixels;
+  SDL_LockSurface(backgroundImage); // Lock surface for direct pixel access capability
+
+  int leftEdgeY = pos.y;
+  int rightEdgeY = pos.y+pos.h;
+  int leftEdgeX = pos.x;
+  int rightEdgeX = pos.x+pos.w;
+
+  for (int curY = leftEdgeY; curY < rightEdgeY; ++curY) {
+    for (int curX = leftEdgeX; curX < rightEdgeX; ++curX) {
+      windowPixels[curY * WIDTH + curX] = bgPixels[curY * WIDTH + curX];
+    }
+  }
+
+  //SDL_BlitSurface(backgroundImage, &pos, windowSurface, &pos);
+  SDL_UnlockSurface(backgroundImage);
+  SDL_UnlockSurface(windowSurface);
+}
+
+int square(int n)
+{
+  // handle negative input
+  if (n < 0) {
+    n = -n;
+  }
+  // Initialize result
+  int res = n;
+
+  // Add n to res n-1 times
+  for (int i = 1; i < n; i++) {
+    res += n;
+  }
+  return res;
+}
+
+SDL_Rect eraseOldCircle() {
+  //Calculate position of ball
+  SDL_Rect pos = {circlePosition.x-radius, circlePosition.y-(radius+paddleSteps), square(radius), radius+radius+paddleSteps};
+  GetBGToFG(pos);
+  return pos;
+}
+
+void drawBox(int startX, int startY, int endX, int endY, SDL_Color color) {
+  uint32_t *pixels = (uint32_t *) windowSurface->pixels;
+  SDL_PixelFormat *windowFormat = windowSurface->format;
+  SDL_LockSurface(windowSurface); // Lock surface for direct pixel access capability
+
+  for (int x = startX; x <= endX; ++x) {
+    for (int y = startY; y <= endY; ++y) {
+      pixels[y * WIDTH + x] = SDL_MapRGB(windowFormat, color.r, color.g, color.b);
+    }
+  }
+  SDL_UnlockSurface(windowSurface);
+}
+
 void drawPaddle(SDL_Rect position, SDL_Color color, int direction) { //direction 0 = left, direction 1 = right
   SDL_Rect prevPosition;
   // Calculate previous position, paddle has only moved the number of steps
   if (direction == 0) {
     prevPosition.x = position.x + paddleSteps;
-    prevPosition.w = position.w + paddleSteps;
   }
   else {
     prevPosition.x = position.x - paddleSteps;
-    prevPosition.w = position.w - paddleSteps;
   }
   prevPosition.y = position.y;
-  prevPosition.h = position.h;
+  prevPosition.h = position.h+1;
+  prevPosition.w = position.w;
   // Copy the part of the background image that the paddle overwrote in the previous position back to the window
-  SDL_BlitSurface(backgroundImage, &prevPosition, windowSurface, &prevPosition);
-  // Fill entire paddle with color
-  SDL_FillRect(paddle, nullptr, SDL_MapRGB(paddle->format, color.r, color.g, color.b));
-  // Copy paddle to screen in the position defined by the position SDL_Rect
-  SDL_BlitSurface(paddle, nullptr, windowSurface, &position);
+  GetBGToFG(prevPosition);
+  // Draw paddle
+  drawBox(paddlePosition.x, paddlePosition.y, paddlePosition.x+paddlePosition.w, paddlePosition.y+paddlePosition.h, color);
   // Apply changes to window
-  SDL_UpdateWindowSurface(window);
-}
-
-void eraseOldCircle() {
-  //Calculate position of ball
-  SDL_Rect pos = {circlePosition.x-(radius+paddleSteps), circlePosition.y-(radius+paddleSteps), radius*radius, radius*2+paddleSteps};
-  SDL_BlitSurface(backgroundImage, &pos, windowSurface, &pos);
+  const SDL_Rect rects[] = {prevPosition, position};
+  SDL_UpdateWindowSurfaceRects(window, rects, 2);
 }
 
 void Circle(int center_x, int center_y, int radius, SDL_Color color) {
-  eraseOldCircle();
+  SDL_Rect pos = {center_x+(radius+paddleSteps), center_y+(radius+paddleSteps), square(radius), square(radius)};
+  SDL_Rect bottom = eraseOldCircle();
+
+  const SDL_Rect rects[] = {pos, bottom};
 
   uint32_t *pixels = (uint32_t *) windowSurface->pixels;
   SDL_PixelFormat *windowFormat = windowSurface->format;
   SDL_LockSurface(windowSurface); // Lock surface for direct pixel access capability
 
-  int radiussqrd = radius * radius;
+  int radiussqrd = square(radius);
 
   for(int x=center_x-radius; x<=center_x+radius; x++) {
-    int dx = center_x - x;
+    int dxsqrd = square(center_x - x);
     for(int y=center_y-radius; y<=center_y+radius; y++) {
-      int dy = center_y - y;
-      if((dy * dy + dx * dx) <= radiussqrd) {
+      int dysqrd = square(center_y - y);
+      if(dysqrd + dxsqrd <= radiussqrd) {
         pixels[(y * WIDTH + x)] = SDL_MapRGB(windowFormat, color.r, color.g, color.b);
       }
     }
   }
 
   SDL_UnlockSurface(windowSurface);
-  SDL_UpdateWindowSurface(window);
+  SDL_UpdateWindowSurfaceRects(window, rects, 2);
 }
 
 void PutResourcesToScreen() {
@@ -150,7 +203,6 @@ void OpenFirstController() {
       }
     }
   }
-  printLn("No controller opened");
 }
 
 void CloseFirstController() {
@@ -163,7 +215,7 @@ void CloseFirstController() {
 void Quit(int status) {
   SDL_FreeSurface(backgroundImage);
   SDL_DestroyWindow(window);
-  SDL_FreeSurface(paddle);
+  //SDL_FreeSurface(paddle);
   CloseFirstController();
   TTF_CloseFont(font);
   TTF_Quit();
@@ -218,7 +270,7 @@ int Init() {
   backgroundImage = LoadBackground(HOME"background.png");
   paddlePosition = {250, 390, 180, 50};
   circlePosition = {345, 60, 0, 0}; // Width and height don't matter here
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+/*#if SDL_BYTEORDER == SDL_BIG_ENDIAN
   Uint32 Rmask = 0x000000FF;
   Uint32 Gmask = 0x0000FF00;
   Uint32 Bmask = 0x00FF0000;
@@ -228,11 +280,10 @@ int Init() {
   Uint32 Rmask = 0x0000FF00;
   Uint32 Gmask = 0x00FF0000;
   Uint32 Bmask = 0xFF000000;
-#endif
-  paddle = SDL_CreateRGBSurface(0, 180, 50, 32, Rmask, Gmask, Bmask, Amask);
-  green = {0, 92, 3};
-  red = {209, 15, 15};
-  blue = {0, 50, 130};
+#endif*/
+  green = {0, 92, 3, 255};
+  red = {209, 15, 15, 255};
+  blue = {0, 50, 130, 255};
   colors[0] = green;
   colors[1] = red;
   colors[2] = blue;
@@ -258,7 +309,12 @@ void ProcessInput() {
       case SDL_CONTROLLERBUTTONDOWN:
         switch (event.cbutton.button) {
           case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
-            paddlePosition.x-=10;
+            if (paddlePosition.x-10 > 0) {
+              paddlePosition.x-=10;
+            }
+            else {
+              paddlePosition.x = 0;
+            }
             drawPaddle(paddlePosition, green, 0);
             break;
           case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
@@ -288,7 +344,12 @@ void ProcessInput() {
       case SDL_KEYDOWN:
         switch (event.key.keysym.sym) {
           case SDLK_LEFT:
-            paddlePosition.x-=10;
+            if (paddlePosition.x-10 > 0) {
+              paddlePosition.x-=10;
+            }
+            else {
+              paddlePosition.x = 0;
+            }
             drawPaddle(paddlePosition, green, 0);
             break;
           case SDLK_RIGHT:
@@ -319,18 +380,19 @@ Uint32 get_pixel32(SDL_Surface *surface, int x, int y ) {
   return pixels[ ( y * surface->w ) + x ];
 }
 
-int compare(SDL_Surface *paddleSurface, SDL_Surface *circleSurface) {
+int compare() {
   SDL_Color circleRGB;
+  SDL_PixelFormat *windowFormat = windowSurface->format;
   // Get pixel data from the circle, whole circle is same color so all pixels are same
-  Uint32 circlePixel = get_pixel32(circleSurface, circlePosition.x, circlePosition.y);
+  Uint32 circlePixel = get_pixel32(windowSurface, circlePosition.x, circlePosition.y);
   // Unlock surface since get_pixel doesn't unlock automatically
-  SDL_UnlockSurface(circleSurface);
-  SDL_GetRGB(circlePixel, windowSurface->format, &circleRGB.r, &circleRGB.g, &circleRGB.b); // Get color from pixel data
+  SDL_UnlockSurface(windowSurface);
+  SDL_GetRGB(circlePixel, windowFormat, &circleRGB.r, &circleRGB.g, &circleRGB.b); // Get color from pixel data
   SDL_Color paddleRGB;
   // Same as circle, but for paddle. Using a different x y value within the paddle's range should have no different result
-  Uint32 paddlePixel = get_pixel32(paddleSurface, 10, 10);
-  SDL_UnlockSurface(paddleSurface);
-  SDL_GetRGB(paddlePixel, paddleSurface->format, &paddleRGB.r, &paddleRGB.g, &paddleRGB.b);
+  Uint32 paddlePixel = get_pixel32(windowSurface, paddlePosition.x, paddlePosition.y);
+  SDL_UnlockSurface(windowSurface);
+  SDL_GetRGB(paddlePixel, windowFormat, &paddleRGB.r, &paddleRGB.g, &paddleRGB.b);
   // Check if colors are same and if the circle is within the paddle's range
   if (paddleRGB.r == circleRGB.r && paddleRGB.g == circleRGB.g && paddleRGB.b == circleRGB.b
   && circlePosition.x > paddlePosition.x && circlePosition.x < (paddlePosition.x + paddlePosition.w)) {
@@ -343,12 +405,12 @@ void startNewCircle() {
   eraseOldCircle();
   circlePosition.y = 60;
   srand(time(nullptr));
-  circlePosition.x = (rand() + radius*2) % 620;
+  circlePosition.x = (rand() + radius*2) % WIDTH-20;
 }
 
 void PrintScore() {
   SDL_Rect scorePos = {0, 0, 140, 20};
-  SDL_BlitSurface(backgroundImage, &scorePos, windowSurface, &scorePos);
+  GetBGToFG(scorePos);
   std::stringstream scoreStream;
   scoreStream << "Score: " << score;
   std::string scoreString = scoreStream.str();
@@ -356,10 +418,11 @@ void PrintScore() {
   scoreStream.str("");
   SDL_BlitSurface(scoreText, nullptr, windowSurface, &scorePos);
   SDL_FreeSurface(scoreText);
+  SDL_UpdateWindowSurfaceRects(window, &scorePos, 1);
 }
 
 void ChangeScore() {
-  if (compare(paddle, windowSurface)) {
+  if (compare()) {
     SDL_QueueAudio(deviceId, wavBuffer, wavLength);
     SDL_PauseAudioDevice(deviceId, 0);
     score++;
